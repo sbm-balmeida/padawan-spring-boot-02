654ae48baeeb9116b32face833c479a8a8540e94
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-23 10:02:44 -0300

    Consulta com filtros

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,9 +1,17 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+//	List<Topico> findByCurso_Nome(String nomeCurso);
+	
+	List<Topico> findByCurso_Nome(String nomeCurso);
+	
+//	@Query("SELECT T FROM Topico T WHERE T.CURSO.NOME = :nomeCurso")
+//	List<Topico> carregarPorNomeDorCurso(@Param("nomeCurso") String nomeCurso);
 }
