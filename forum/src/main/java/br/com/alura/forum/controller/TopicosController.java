79477f51bf2970d4590ed915f17e399bcf42f06f
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-24 10:11:41 -0300

    Removendo tópicos

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -62,6 +63,7 @@ public class TopicosController {
 	}
 	
 	@GetMapping("/{id}")
+	@Transactional
 	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
 		Topico topico = topicoRepository.getReferenceById(id);
 		
@@ -75,5 +77,13 @@ public class TopicosController {
 		Topico topico = form.atualizar(id, topicoRepository);
 		
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build();
 	}
 }
